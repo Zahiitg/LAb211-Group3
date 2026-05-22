84d632d3f06de2995db750c3c41d7d08fc17dc62
Zahiitg <155324337+Zahiitg@users.noreply.github.com>
2026-05-22 15:08:52 +0700

    Add files via upload

---
+++ src/model/enums/LockMechanism.java
@@ -0,0 +1,5 @@
+package model.enums;
+
+public enum LockMechanism {
+    NO_LOCK, FILE_LOCK, SYNCHRONIZED, OPTIMISTIC
+}
---
+++ src/model/enums/OrderStatus.java
@@ -0,0 +1,5 @@
+package model.enums;
+
+public enum OrderStatus {
+    PENDING, SUCCESS, FAILED_OUT_OF_STOCK, FAILED_RETRY_EXCEEDED
+}
